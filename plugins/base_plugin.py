bad904a96c3365cc4198e667fabb71365050dc2d
4SMx64 <heshansankalpa2004@gmail.com>
2026-05-31 08:51:08 +0530

    Update base_plugin.py

--- plugins/base_plugin.py
+++ plugins/base_plugin.py
@@ -3,15 +3,8 @@ from typing import Dict, Any
 
 class BasePlugin:
     @property
-    def name(self) -> str:
-        raise NotImplementedError
-
+    def name(self) -> str: raise NotImplementedError
     @property
-    def description(self) -> str:
-        raise NotImplementedError
-
-    def setup_arguments(self, parser: argparse.ArgumentParser) -> None:
-        pass
-
-    def execute(self, args: argparse.Namespace, config: Dict[str, Any]) -> int:
-        raise NotImplementedError
+    def description(self) -> str: raise NotImplementedError
+    def setup_arguments(self, parser: argparse.ArgumentParser) -> None: pass
+    def execute(self, args: argparse.Namespace, config: Dict[str, Any]) -> int: raise NotImplementedError
